45b39c60fdebda94d4188f7ddf5bfbfdbf8f2934
sentius79 <claudiumarinescu@yahoo.com>
2022-06-29 13:23:18 +0300

    Added basic table.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-
+import { MainPageComponent } from './main-page/main-page.component';
+import { MatTableModule } from '@angular/material/table'
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainPageComponent
+  ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatTableModule
   ]
 })
 export class DashboardModule { }
